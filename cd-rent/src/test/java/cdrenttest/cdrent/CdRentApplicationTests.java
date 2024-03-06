8731e2d0ee775dbf3116cf82314dea1c21d8da18
Victor Gomes <victorgomescorrea19@gmail.com>
2024-03-06 17:17:39 -0300

    Basic folders created

---
+++ cd-rent/src/test/java/cdrenttest/cdrent/CdRentApplicationTests.java
@@ -0,0 +1,13 @@
+package cdrenttest.cdrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
