c176718f9c99ab98e03cc3c2e48d9115afce3048
Victor Gomes <victorgomescorrea19@gmail.com>
2024-03-08 17:24:31 -0300

    Cd controller created

--- cd-rent/src/main/java/cdrenttest/cdrent/controller/CdController.java
+++ cd-rent/src/main/java/cdrenttest/cdrent/controller/CdController.java
@@ -1,2 +1,29 @@
-package cdrenttest.cdrent.controller;public class CdController {
+package cdrenttest.cdrent.controller;
+
+import cdrenttest.cdrent.domain.Cd;
+import cdrenttest.cdrent.service.CdService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("cd")
+@RequiredArgsConstructor
+public class CdController {
+    private final CdService cdService;
+
+    @GetMapping("/{title}")
+    public ResponseEntity<Cd> findCdByName(@PathVariable String title) {
+        return ResponseEntity.ok(cdService.findCdByTitle(title));
+    }
+
+    @GetMapping("/find/{title}")
+    public ResponseEntity<List<Cd>> searchByTitle(@PathVariable String title) {
+        return ResponseEntity.ok(cdService.searchByTitle(title));
+    }
 }
